ef2c8cf9e7d1f2f2a2d1617873e1eafedf69ea75
KasaiRaito <eduardo.huerta@saei.mx>
2026-02-18 21:49:23 -0600

    Scene Loader

---
+++ Autoload/transition.gd
@@ -0,0 +1,14 @@
+extends Node
+
+@onready var effect: ColorRect = %Effect
+
+func trnsition_to(scene_path: String) -> void:
+	var tween := create_tween();
+	tween.tween_property(effect.material, "shader_parameter/progress", 1.0, 1.0);
+	
+	await tween.finished;
+	get_tree().change_scene_to_file(scene_path);
+	
+	tween = create_tween()
+	tween.tween_property(effect.material, "shader_parameter/progress", 0.0, 1.0);
+	
--- scenes/UI/main_menu.gd
+++ scenes/UI/main_menu.gd
@@ -1,3 +1,37 @@
 extends Control
-
 class_name MainMenu
+
+@onready var main_buttons: Control = $MainButtons
+@onready var settings_buttons: Control = $SettingsButtons
+
+func _on_play_button_pressed() -> void:
+	Transition.trnsition_to("res://Scenes/UI/character_selection.tscn")
+
+func _on_settings_button_pressed() -> void:
+	var tween := create_tween();
+	tween.tween_property(main_buttons, "global_position:y", 350.0, 0.2);
+	tween.tween_interval(0.1);
+	tween.tween_property(settings_buttons, "global_position:x", 145.0, 0.3);
+
+
+func _on_quit_button_pressed() -> void:
+	pass; # Replace with function body.
+
+
+func _on_music_button_pressed() -> void:
+	pass; # Replace with function body.
+
+
+func _on_sfx_button_pressed() -> void:
+	pass; # Replace with function body.
+
+
+func _on_window_button_pressed() -> void:
+	pass; # Replace with function body.
+
+
+func _on_back_button_pressed() -> void:
+	var tween := create_tween();
+	tween.tween_property(settings_buttons, "global_position:x", 580.0, 0.3);
+	tween.tween_interval(0.1);
+	tween.tween_property(main_buttons, "global_position:y", 94.0, 0.2);
