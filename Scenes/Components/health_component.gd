b4322632c77a59bfff08ae0b7b7df38f48dd2aa8
KasaiRaito <eduardo.huerta@saei.mx>
2026-02-26 19:37:37 -0600

    AddHealthComponent

---
+++ Scenes/Components/health_component.gd
@@ -0,0 +1,33 @@
+extends Node2D
+class_name HealthComponent
+
+signal on_unit_damage(amount: float)
+signal on_unit_heal(amount: float)
+signal on_unit_dead
+
+var current_health: float
+var max_helath: float
+
+func init_health(value: float) -> void:
+	current_health = value
+	max_helath = value
+
+func take_damage(value: float) -> void:
+	if current_health > 0:
+		current_health -= abs(value)
+		current_health = clamp(current_health , 0.0, max_helath)
+		on_unit_damage.emit(abs(value))
+		
+		if current_health == 0:
+			die()
+
+func die() -> void:
+	on_unit_dead.emit()
+
+func heal(value: float) -> void:
+	if current_health >= max_helath:
+		return
+	
+	current_health += abs(value)
+	current_health = clamp(current_health , 0.0, max_helath)
+	on_unit_heal.emit(value)
