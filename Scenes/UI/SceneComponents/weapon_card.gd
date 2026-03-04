831419b5b67eda5be149c8e1b636c7f32f22605d
KasaiRaito <eduardo.huerta@saei.mx>
2026-03-03 20:22:27 -0600

    AddDescriptions

--- Scenes/UI/SceneComponents/weapon_card.gd
+++ Scenes/UI/SceneComponents/weapon_card.gd
@@ -4,6 +4,7 @@ class_name WeaponCard
 @onready var icon: TextureRect = $Icon
 @onready var hover_sound: AudioStreamPlayer = $HoverSound
 @onready var selector: TextureRect = $Selector
+@onready var description_panel: DescriptionPanel = $DescriptionPanel
 
 var data: WeaponData
 
@@ -13,7 +14,23 @@ func set_data(value: WeaponData) -> void:
 	
 	data = value
 	icon.texture = data.icon
+	
+	set_description()
+
+func set_description() -> void:
+	if not data:
+		return
+	description_panel.set_text(data.description)
 
 func _on_mouse_entered() -> void:
 	hover_sound.play()
 	DampedOscillator.animate(icon, "scale", randf_range(400.0, 450.0), randf_range(5.0, 10.0), randf_range(10.0,15.0), 0.5)
+	
+	description_panel.show()
+	DampedOscillator.animate(description_panel, "scale", randf_range(400.0, 450.0), randf_range(5.0, 10.0), randf_range(5.0,10.0), 0.25)
+	DampedOscillator.animate(description_panel, "rotation_degrees", 300.0, 7.5, 15, 0.5 * randf_range(-20.0,20))
+
+
+func _on_mouse_exited() -> void:
+	description_panel.hide()
+	
